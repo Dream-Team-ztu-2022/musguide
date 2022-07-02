1969518ae6c14ec4bec83fb22b226ca1b6761c8d
Illia Kliuiev <i.kliuiev@easygenerator.com>
2022-07-02 16:47:50 +0300

    chore: remove todo comment

--- src/api/MusicBrainzApi.ts
+++ src/api/MusicBrainzApi.ts
@@ -1,7 +1,6 @@
 import { Api } from "./Api";
 
 export class MusicBrainzApi {
-  // TODO: Write me the interfaces
   static getArtist = (mbid: string) => Api.get<any>(`https://musicbrainz.org/ws/2/artist/${mbid}?inc=url-rels&fmt=json`);
 
   static getSong = (mbid: string) => Api.get<any>(`https://musicbrainz.org/ws/2/song/${mbid}?inc=url-rels&fmt=json`);
